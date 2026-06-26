b760804dc93cdd4cb9072877f048b75380b11695
Gemma Maeso <gemmaa.dev@gmail.com>
2026-06-26 12:08:59 +0200

    feat(data): add mock images array

---
+++ src/components/Gallery.tsx
@@ -0,0 +1,4 @@
+ //Component pare
+// Passa dades via props: A Gallery, defineix un array d'objectes amb id i src (utilitza imatges de picsum.photos).
+// Destaca la primera imatge: A Gallery, condiciona que el primer element del array tingui una prop isFeatured={true}.
+
---
+++ src/components/ImageItem.tsx
@@ -0,0 +1,5 @@
+//Component fill
+// Passa dades via props: Passa cada objecte com a prop a ImageItem. Crea un type per tipar les props que estàs passant.
+// Destaca la primera imatge: A ImageItem, usa isFeatured per renderitzar condicionalment una classe de CSS que dupliqui la mida de l'element destacat.
+
+
