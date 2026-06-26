f882275af0f5a1f74797dff87fe0d4c1722f95c1
Gemma Maeso <gemmaa.dev@gmail.com>
2026-06-26 10:59:58 +0200

    feat: organize initial project folder structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// Import d'App i styles
 
-createRoot(document.getElementById('root')!).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./styles/index.css"; 
+
+const root = createRoot(document.getElementById("root")!);
+
+root.render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
