a422163205eb53cfd774556eed658467794972e9
Gemma Maeso <gemmaa.dev@gmail.com>
2026-06-27 18:00:47 +0200

    chore: install and configure shadcn/ui

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { Slot } from "radix-ui";
+
+import { cn } from "@/utils";
+import { buttonVariants, type ButtonVariants } from "./button.variants";
+
+function Button({
+  className,
+  variant = "default",
+  size = "default",
+  asChild = false,
+  ...props
+}: React.ComponentProps<"button"> &
+  ButtonVariants & {
+    asChild?: boolean;
+  }) {
+  const Comp = asChild ? Slot.Root : "button";
+
+  return (
+    <Comp
+      data-slot="button"
+      data-variant={variant}
+      data-size={size}
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    />
+  );
+}
+
+export { Button };
